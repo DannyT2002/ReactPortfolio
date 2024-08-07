3cacc6de774b66726038c7edbc42b9eb953264ef
Daniel Torres <torres.daniel.0827@gmail.com>
2024-08-06 20:20:35 -0500

    created index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
+
