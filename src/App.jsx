3493c2d850dcae34a1e9b0891918ef353ce21490
Daniel Torres <torres.daniel.0827@gmail.com>
2024-08-09 19:10:50 -0500

    added css

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import AboutMe from './AboutMe';
-import Portfolio from './Portfolio';
-import Contact from './Contact';
-import Resume from './Resume';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
 
 const App = () => (
   <div>
     <Header />
     <main>
-      <AboutMe />
+      <About />
       <Portfolio />
       <Contact />
       <Resume />
---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About = () => (
+  <section id="about">
+    <img src="your-photo.jpg" alt="Your Avatar" />
+    <p>Short bio about yourself.</p>
+  </section>
+);
+
+export default About;
