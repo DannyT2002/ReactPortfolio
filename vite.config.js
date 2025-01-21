663e40e9fb8f8d5fceaa4e08df41b0fbba9eae6a
Daniel Torres <torres.daniel.0827@gmail.com>
2025-01-20 21:27:24 -0600

    Fixed config file

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, // Local development port
+    port: 3000,
     open: true,
-    // Ensure the server listens on all network interfaces
     host: true,
+    allowedHosts: ['reactportfolio-7lw8.onrender.com'],
   },
   build: {
     outDir: 'dist',
